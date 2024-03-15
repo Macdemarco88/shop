3a118471233b2177fdbe981d53b848c06e6d3871
Macdemarco88 <thomastrain39@gmail.com>
2024-03-16 02:38:16 +0900

    16.03.24

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,21 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
-let YellowBtn = styled.button`
-  background: yellow;
-  color: black;
-  padding: 10px;
-`;
-
 function Detail(props) {
+  let [hideAlert, setHide] = useState(false);
+  let [num, setNum] = useState("");
   let { id } = useParams();
-  console.log(id);
   let foundId = props.records.find((a) => a.id == id);
-  console.log(foundId);
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      setHide(true);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      alert("그러지마쇼");
+    }
+  }, [num]); // num이 변결될 때만 실행
 
   return (
     <div className="container">
-      <YellowBtn>버튼</YellowBtn>
+      {!hideAlert && (
+        <div className="alert alert-warning">2초이내 구매시 뽀뽀해드림</div>
+      )}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -24,6 +34,11 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }} // 인풋에 값 입력 하면 num 을 state에 저장
+          />
           <h4 className="pt-5">{[foundId.title]}</h4>
           <p>{[foundId.content]}</p>
           <p>{[foundId.price]}</p>
